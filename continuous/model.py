030a25a969ee44ef792697de2b2839791bff6882
Claudio Noguera <claudio.noguera@gmail.com>
2020-05-16 11:22:29 +0200

    change default network size to 512 and 256

--- continuous/model.py
+++ continuous/model.py
@@ -13,8 +13,8 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=256,
-                 fc2_units=128):
+    def __init__(self, state_size, action_size, seed, fc1_units=512,
+                 fc2_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -46,8 +46,8 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=256,
-                 fc2_units=128):
+    def __init__(self, state_size, action_size, seed, fcs1_units=512,
+                 fc2_units=256):
         """Initialize parameters and build model.
         Params
         ======
